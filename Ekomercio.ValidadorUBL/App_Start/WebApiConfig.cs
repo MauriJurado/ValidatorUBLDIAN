70d42c3ebc63e4fd7a51d3101991a7a5fa97d932
MauriJurado <37162668+MauriJurado@users.noreply.github.com>
2019-03-25 09:59:27 -0600

    Add files via upload

---
+++ Ekomercio.ValidadorUBL/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace Ekomercio.ValidadorUBL
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+            config.MapHttpAttributeRoutes();
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            // Rutas de API web
+            config.Routes.MapHttpRoute(
+                name: "Metodos",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
